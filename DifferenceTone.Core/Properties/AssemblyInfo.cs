9a635b50f876e157805b0293360cbc2637db8e54
Johannes Schmitt <johannes.schmitt@live.com>
2017-03-05 19:33:35 +0100

    create a shared assemblyInfo file

--- DifferenceTone.Core/Properties/AssemblyInfo.cs
+++ DifferenceTone.Core/Properties/AssemblyInfo.cs
@@ -1,7 +1,5 @@
 ﻿using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -9,22 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DifferenceTone.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Johannes Schmitt")]
 [assembly: AssemblyProduct("DifferenceTone.Core")]
-[assembly: AssemblyCopyright("Johannes Schmitt")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
